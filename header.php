be089a8d8175e60975a36c993e89c0dcb23c5452
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-10-27 16:34:36 +0100

    adding login and register forms

--- header.php
+++ header.php
@@ -1,3 +1,5 @@
+<?php ?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -31,18 +33,16 @@
                     <nav class="navbar navbar-default">
                         <div class="container-fluid">
                             <div class="navbar-header">
-                                <a class="navbar-brand" href="#">Home</a>
+                                <a class="navbar-brand" href="index.php">Home</a>
                             </div>
                             <ul class="nav navbar-nav">
                                 <li class="active"><a href="#">LINK</a></li>
                                 <li><a href=".#">LINK</a></li>
-                                <li><a href="#">LINK</a></li>
-                                <li><a href="#">LINK</a></li>
                             </ul>
 
                             <ul class="nav navbar-nav navbar-right">
-                                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Register</a></li>
-                                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                                <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Register</a></li>
+                                <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                             </ul>
                         </div>
                     </nav>
@@ -50,5 +50,3 @@
             </div>
             <hr>
     </div>
-
-<?php ?>
