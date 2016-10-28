050b2264173979f1a43d9376cded3fa33693035a
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-10-28 19:00:33 +0100

    first draft register - not functional
    Error Code: 1136. Column count doesn't match value count at row 1

---
+++ registerUser.php
@@ -0,0 +1,25 @@
+<?php
+include 'dbConnect.php';
+
+$customer_id = 1;
+$username = $_POST["username"];
+$password = $_POST["password"];
+$title = $_POST["title"];
+$forename = $_POST["forename"];
+$surname = $_POST["surname"];
+$street = $_POST["street"];
+$county = $_POST["county"];
+$city = $_POST["city"];
+$post_code = $_POST["post_code"];
+$contact_phone_region = $_POST["contact_phone_region"];
+$contact_phone = $_POST["contact_phone"];
+$contact_email = $_POST["contact_email"];
+
+
+$sqlCommand = "INSERT INTO customer(customer_id, title, forename, surname, street, county, city, post_code, contact_phone_region, contact_phone, contact_email, username, password) VALUES ('$customer_id', '$forename', '$surname', '$street', '$county', '$city', '$post_code', '$contact_phone_region', '$contact_phone', '$contact_email', '$username', '$password')";
+
+$query = mysqli_query($db, $sqlCommand);
+
+echo "$sqlCommand <br>";
+echo "Successfully added to register";
+?>
