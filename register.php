48250b84c0b894dd65d2e1f6274883db954e56f1
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-10-28 19:46:22 +0100

    registration works now, database error fixed and register page updated - basic only

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-1"></div>
             <div class="col-md-10">
-                <form name="Register on delivrhyno" method="POST" action="registerUser.php" class="form-horizontal">
+                <form name="Register" method="POST" action="registerUser.php" class="form-horizontal">
                     <fieldset>
 
                         <div class="form-group">
@@ -28,7 +28,7 @@
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="contact_email">Contact Email</label>
                             <div class="col-md-4">
-                                <input id="contact_email" name="contact_email" type="email" placeholder="contact_email"
+                                <input id="contact_email" name="contact_email" type="email" placeholder="contact email"
                                        class="form-control input-md" required>
                             </div>
                         </div>
@@ -87,7 +87,7 @@
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="post_code">Post Code</label>
                             <div class="col-md-4">
-                                <input id="post_code" name="post_code" type="text" placeholder="post_code"
+                                <input id="post_code" name="post_code" type="text" placeholder="post code"
                                        class="form-control input-md" required>
                             </div>
                         </div>
@@ -97,14 +97,14 @@
                                 Region</label>
                             <div class="col-md-4">
                                 <input id="contact_phone_region" name="contact_phone_region" type="text"
-                                       placeholder="contact_phone_region" class="form-control input-md" required>
+                                       placeholder="contact phone region" class="form-control input-md" required>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="contact_phone">Contact Phone</label>
                             <div class="col-md-4">
-                                <input id="contact_phone" name="contact_phone" type="text" placeholder="contact_phone"
+                                <input id="contact_phone" name="contact_phone" type="text" placeholder="contact phone"
                                        class="form-control input-md" required>
                             </div>
                         </div>
--- registerUser.php
+++ registerUser.php
@@ -1,24 +1,32 @@
+<?php include 'header.php'; ?>
+
 <?php
-include 'dbConnect.php';
+function registerUser() {
 
-$username = $_POST["username"];
-$password = $_POST["password"];
-$title = $_POST["title"];
-$forename = $_POST["forename"];
-$surname = $_POST["surname"];
-$street = $_POST["street"];
-$county = $_POST["county"];
-$city = $_POST["city"];
-$post_code = $_POST["post_code"];
-$contact_phone_region = $_POST["contact_phone_region"];
-$contact_phone = $_POST["contact_phone"];
-$contact_email = $_POST["contact_email"];
+    include 'dbConnect.php';
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $title = $_POST["title"];
+    $forename = $_POST["forename"];
+    $surname = $_POST["surname"];
+    $street = $_POST["street"];
+    $county = $_POST["county"];
+    $city = $_POST["city"];
+    $post_code = $_POST["post_code"];
+    $contact_phone_region = $_POST["contact_phone_region"];
+    $contact_phone = $_POST["contact_phone"];
+    $contact_email = $_POST["contact_email"];
 
 
-$sqlCommand = "INSERT INTO customer(title, forename, surname, street, county, city, post_code, contact_phone_region, contact_phone, contact_email, username, password) VALUES ('$title', '$forename', '$surname', '$street', '$county', '$city', '$post_code', '$contact_phone_region', '$contact_phone', '$contact_email', '$username', '$password')";
+    $sqlCommand = "INSERT INTO customer(title, forename, surname, street, county, city, post_code, contact_phone_region, contact_phone, contact_email, username, password) VALUES ('$title', '$forename', '$surname', '$street', '$county', '$city', '$post_code', '$contact_phone_region', '$contact_phone', '$contact_email', '$username', '$password')";
 
-$query = mysqli_query($db, $sqlCommand);
-
-echo "$sqlCommand <br>";
-echo "Successfully added to register";
+    $query = mysqli_query($db, $sqlCommand);
+}
+    registerUser();
 ?>
+
+<h1>
+    Thanks for registering!
+</h1>
+<?php include 'footer.php'; ?>
