d46d2bd17f27d5011250172ce1fa30a6bf1d97b6
IainMorton <IainMorton@computing.dundee.ac.uk>
2016-10-31 13:34:04 +0000

    The Order Status

---
+++ OrderStatus.php
@@ -0,0 +1,12 @@
+<?php include 'header.php';?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Welcome to delivrhyno</h2>
+            <p>OrderStatus</p>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php';?>
