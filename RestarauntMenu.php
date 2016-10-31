6660170d9ef77420f606c6128770349ffbe7c07f
IainMorton <IainMorton@computing.dundee.ac.uk>
2016-10-31 13:31:21 +0000

    The Restaraunt Menu Display

---
+++ RestarauntMenu.php
@@ -0,0 +1,12 @@
+<?php include 'header.php';?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Welcome to delivrhyno</h2>
+            <p>Menu</p>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php';?>
