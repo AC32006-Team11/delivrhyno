330f7bc91d96d4960f6f941e000613d5b3c43907
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-10-31 12:38:12 +0000

    update

--- restaurantsFull.php
+++ restaurantsFull.php
@@ -1,9 +1,10 @@
 <?php include 'header.php'; ?>
 <?php include 'dbConnect.php'; ?>
-<?php session_start();
+<?php
 //INSERT INTO anthonygrimesdb.restaurant(restaurant_description, street, county, city, post_code, contact_phone_region, contact_phone, delivery_time) VALUES ('','','','','','','','');
 //INSERT INTO anthonygrimesdb.restaurant_menu_item(restaurant_id,menu_item_group,menu_item_name,menu_item_price) VALUES ('','','','');
 ?>
+
 
 
 
