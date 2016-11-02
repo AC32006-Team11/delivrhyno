e6bd920fe80b926682ac3d0fae9b558d97a41495
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-11-02 00:33:00 +0000

    todo update

--- aboutUs.php
+++ aboutUs.php
@@ -4,8 +4,16 @@
     <div class="row">
         <div class="col-md-12">
             <h2>About Us:</h2>
-			<br>
-            <p class="text-justify">Delivrhyno is a national business based in the UK, specialising in providing take-away delivery services, in particular allowing our customers to order food from local establishments that may not offer their own take-away service. Delivrhyno cooperates with these establishments directly via our branch offices, allowing restaurants to always have up-to-date information available on our website. Customers can use the Delivrhyno website to choose menu items from any participating establishment in their area. Once an order is complete and paid for by the customer, it is then communicated to the restaurant which can then begin fulfilling the order. Once confirmation that the order is ready, a Delivrhyno driver will pick up the order and it will be transported to the customer's location.</p>
+            <br>
+            <p class="text-justify">Delivrhyno is a national business based in the UK, specialising in providing
+                take-away delivery services, in particular allowing our customers to order food from local
+                establishments that may not offer their own take-away service. Delivrhyno cooperates with these
+                establishments directly via our branch offices, allowing restaurants to always have up-to-date
+                information available on our website. Customers can use the Delivrhyno website to choose menu items from
+                any participating establishment in their area. Once an order is complete and paid for by the customer,
+                it is then communicated to the restaurant which can then begin fulfilling the order. Once confirmation
+                that the order is ready, a Delivrhyno driver will pick up the order and it will be transported to the
+                customer's location.</p>
         </div>
     </div>
 </div>
