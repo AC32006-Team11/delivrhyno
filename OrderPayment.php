10fcc2e94835fbc41da81bf11edd44741eab4dfe
IainMorton <IainMorton@computing.dundee.ac.uk>
2016-11-04 11:34:37 +0000

    Renamed to userPay.php to orderPayment.php

--- OrderPayment.php
+++ OrderPayment.php
@@ -1,8 +1,11 @@
 <?php include 'header.php'; ?>
+<?php include 'dbConnect.php'; ?>
 
 <div class="container">
     <div class="row">
 		<div class="col-md-6">
+		<?php
+		if ( isset($_SESSION["loggedIn"]) == "loggedIn") { ?>
 		<h2>Complete Your Order</h2> <!--Is someone going to pass through a total price to this page so that I could fill it up?-->
 				<br>
 				<p>Use the dropdown box below to select the method that you would like to complete the payment with.</p>
@@ -24,6 +27,9 @@
 			
 			<p style="color:grey;"><i>Select 'Pay at the door' to make your payment when the order is delivered to your house. Selecting this will immediately direct you to the order confirmation page.</i></p>
 			<p style="color:grey;"><i>With Paypal, you will be redirected to the Paypal Website were you can make a secure transaction. Once completed you will be redirected to the order confirmation page.</i></p>
+			<?php }  else {
+				echo'<h1>No User Found</h1>';
+			} ?>
 		</div>
 	</div>
 </div>
