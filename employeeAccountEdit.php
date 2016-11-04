efdd3c4b653579e97bedca149294153512280eb7
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-11-04 12:38:50 +0000

    basic employee pages created

---
+++ employeeAccountEdit.php
@@ -0,0 +1,108 @@
+<?php include 'header.php'; ?>
+
+<?php if (isset($_SESSION["empLoggedIn"]) == "empLoggedIn") { ?>
+<div class="container">
+    <div class="col-md-4">
+        <h2>Please enter the details you wish to update</h2>
+        <br>
+    </div>
+
+    <form method="POST" action="employeeAccountUpdate.php" class="form-horizontal">
+        <fieldset>
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="contact_email">Contact Email</label>
+                <div class="col-md-6">
+                    <input id="contact_email" name="contact_email" type="email" placeholder="contact email"
+                           class="form-control input-md">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="title">Title</label>
+                <div class="col-md-6">
+                    <input id="title" name="title" type="text" placeholder="title"
+                           class="form-control input-md">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="forename">Forename</label>
+                <div class="col-md-6">
+                    <input id="forename" name="forename" type="text" placeholder="forename"
+                           class="form-control input-md">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="surname">Surname</label>
+                <div class="col-md-6">
+                    <input id="surname" name="surname" type="text" placeholder="surname"
+                           class="form-control input-md">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="street">Street</label>
+                <div class="col-md-6">
+                    <input id="street" name="street" type="text" placeholder="street"
+                           class="form-control input-md">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="county">County</label>
+                <div class="col-md-6">
+                    <input id="county" name="county" type="text" placeholder="county"
+                           class="form-control input-md">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="city">City</label>
+                <div class="col-md-6">
+                    <input id="city" name="city" type="text" placeholder="city"
+                           class="form-control input-md">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="post_code">Post Code</label>
+                <div class="col-md-6">
+                    <input id="post_code" name="post_code" type="text" placeholder="post code"
+                           class="form-control input-md">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="contact_phone_region">Contact Phone
+                    Region</label>
+                <div class="col-md-6">
+                    <input id="contact_phone_region" name="contact_phone_region" type="text"
+                           placeholder="contact phone region" class="form-control input-md">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="contact_phone">Contact Phone</label>
+                <div class="col-md-6">
+                    <input id="contact_phone" name="contact_phone" type="text" placeholder="contact phone"
+                           class="form-control input-md">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="Update Account"></label>
+                <div class="col-md-1">
+                    <button type="submit" value="UpdateAccount" id="UpdateAccount" name="UpdateAccount"
+                            class="btn btn-success">Update Account
+                    </button>
+                </div>
+            </div>
+        </fieldset>
+    </form>
+
+    <?php } else {
+        echo '<h2>No User Found</h2>';
+    } ?>
+
+    <?php include 'footer.php'; ?>
---
+++ employeeLogin.php
@@ -0,0 +1,30 @@
+<?php include 'header.php'; ?>
+
+<?php
+include('dbConnect.php');
+
+
+//still requires role filtering
+if (isset($_POST['username']) and isset($_POST['password'])) {
+
+    $username = $_POST['username'];
+    $passwordInsecure = $_POST["password"];
+    $password = sha1($passwordInsecure);
+
+    $query = "SELECT * FROM `employee` WHERE username = '$username' and password='$password'";
+    $result = mysqli_query($db, $query) or die(mysqli_error($db));
+
+    $row = mysqli_fetch_array($result);
+
+    $count = mysqli_num_rows($result);
+    if ($count == 1) {
+        $_SESSION['empLoggedIn'] = "empLoggedIn";
+        $_SESSION['username'] = $username;
+        echo "Valid Login Credentials.";
+    }
+} else {
+    echo "Invalid Login Credentials.";
+}
+?>
+
+<?php include 'footer.php'; ?>
