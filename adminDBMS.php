2a4af918fb89c9a0fe7c8371e5fb5e07b9fa655c
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-11-04 15:25:55 +0000

    fucking branches man

--- adminDBMS.php
+++ adminDBMS.php
@@ -1,4 +1,14 @@
 <?php include 'header.php'; ?>
 <a href="adminDBMSRegisterEmployee.php">Register a new employee</a>
+<br>
+<a href="adminDBMSRegisterBranch.php">Register a new branch</a>
+<br>
+<a href="">Update an employee</a>
+<br>
+<a href="">Update a branch</a>
+<br>
+<a href="">Remove an employee</a>
+<br>
+<a href="">Remove a branch</a>
 
 <?php include 'footer.php'; ?>
