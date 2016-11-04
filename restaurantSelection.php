193ac08cb787efd1856cc8fd5601e16f189d3ee6
Craigieboy10 <craig-watt@live.co.uk>
2016-11-04 11:57:51 +0000

    changed database name and small changes to rest select

--- restaurantSelection.php
+++ restaurantSelection.php
@@ -1,11 +1,14 @@
 <?php include 'header.php'; ?>
 <?php include 'dbConnect.php'; ?>
 <?php
-//INSERT INTO anthonygrimesdb.restaurant(restaurant_description, street, county, city, post_code, contact_phone_region, contact_phone, delivery_time) VALUES ('','','','','','','','');
-//INSERT INTO anthonygrimesdb.restaurant_menu_item(restaurant_id,menu_item_group,menu_item_name,menu_item_price) VALUES ('','','','');
+//$sqlquery = "INSERT INTO craigwattdb.restaurant(restaurant_name, restaurant_description, street, county, city, post_code, contact_phone_region, contact_phone, delivery_time) VALUES ('Hong Kong','The finest Chinese food ever served','','','','','','','');";
+
+//$sqlquery2 ="INSERT INTO craigwattdb.restaurant_menu_item(restaurant_id,menu_item_group,menu_item_name,menu_item_price) VALUES ('','','','');"; 
+
+//$sqlGetRestName
+//$sqlGetRestInfo = "SELECT FROM craigwattdb.restaurant"
+//$result = mysqli_query($db, $sqlquery) or die(mysqli_error($db));
+//$result = mysqli_query($db, $sqlquery) or die(mysqli_error($db));
 ?>
 
 
-
-
-<?php include 'footer.php'; ?>
