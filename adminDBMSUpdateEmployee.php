44bcff3a6cc9b241b156099f9d6df7dd5b536bf8
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-11-05 01:06:37 +0000

    employee update

--- adminDBMSUpdateEmployee.php
+++ adminDBMSUpdateEmployee.php
@@ -4,14 +4,22 @@
         <div class="row">
             <div class="col-md-1"></div>
             <div class="col-md-10">
-                <form name="Register" method="POST" action="" class="form-horizontal">
+                <form name="Register" method="POST" action="DBMSUpdateEmployee.php" class="form-horizontal">
                     <fieldset>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="employeeID">Employee ID</label>
+                            <div class="col-md-4">
+                                <input id="employeeID" name="employeeID" type="text"
+                                       placeholder="Employee ID" class="form-control input-md">
+                            </div>
+                        </div>
 
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="branchID">Branch ID</label>
                             <div class="col-md-4">
                                 <input id="branchID" name="branchID" type="text"
-                                       placeholder="branchID" class="form-control input-md" required>
+                                       placeholder="Branch ID" class="form-control input-md">
                             </div>
                         </div>
 
@@ -19,25 +27,7 @@
                             <label class="col-md-4 control-label" for="role">Role</label>
                             <div class="col-md-4">
                                 <input id="role" name="role" type="text"
-                                       placeholder="role" class="form-control input-md" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="username">Username</label>
-                            <div class="col-md-4">
-                                <input pattern=".{5,12}" id="username" name="username" type="text"
-                                       placeholder="username" class="form-control input-md" required
-                                       title="username must be between 5 to 12 characters long">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="password">Password</label>
-                            <div class="col-md-4">
-                                <input pattern=".{8,16}" id="password" name="password" type="password"
-                                       placeholder="password" class="form-control input-md" required
-                                       title="password must be between 8 to 16 characters long">
+                                       placeholder="role" class="form-control input-md">
                             </div>
                         </div>
 
@@ -45,7 +35,7 @@
                             <label class="col-md-4 control-label" for="contact_email">Contact Email</label>
                             <div class="col-md-4">
                                 <input id="contact_email" name="contact_email" type="email" placeholder="contact email"
-                                       class="form-control input-md" required>
+                                       class="form-control input-md">
                             </div>
                         </div>
 
@@ -53,7 +43,7 @@
                             <label class="col-md-4 control-label" for="title">Title</label>
                             <div class="col-md-4">
                                 <input id="title" name="title" type="text" placeholder="title"
-                                       class="form-control input-md" required>
+                                       class="form-control input-md">
                             </div>
                         </div>
 
@@ -61,7 +51,7 @@
                             <label class="col-md-4 control-label" for="forename">Forename</label>
                             <div class="col-md-4">
                                 <input id="forename" name="forename" type="text" placeholder="forename"
-                                       class="form-control input-md" required>
+                                       class="form-control input-md">
                             </div>
                         </div>
 
@@ -69,7 +59,7 @@
                             <label class="col-md-4 control-label" for="surname">Surname</label>
                             <div class="col-md-4">
                                 <input id="surname" name="surname" type="text" placeholder="surname"
-                                       class="form-control input-md" required>
+                                       class="form-control input-md">
                             </div>
                         </div>
 
@@ -77,8 +67,7 @@
                             <label class="col-md-4 control-label" for="street">Street</label>
                             <div class="col-md-4">
                                 <input id="street" name="street" type="text" placeholder="street"
-                                       class="form-control input-md"
-                                       required>
+                                       class="form-control input-md">
                             </div>
                         </div>
 
@@ -86,8 +75,7 @@
                             <label class="col-md-4 control-label" for="county">County</label>
                             <div class="col-md-4">
                                 <input id="county" name="county" type="text" placeholder="county"
-                                       class="form-control input-md"
-                                       required>
+                                       class="form-control input-md">
                             </div>
                         </div>
 
@@ -95,8 +83,7 @@
                             <label class="col-md-4 control-label" for="city">City</label>
                             <div class="col-md-4">
                                 <input id="city" name="city" type="text" placeholder="city"
-                                       class="form-control input-md"
-                                       required>
+                                       class="form-control input-md">
                             </div>
                         </div>
 
@@ -104,7 +91,7 @@
                             <label class="col-md-4 control-label" for="post_code">Post Code</label>
                             <div class="col-md-4">
                                 <input id="post_code" name="post_code" type="text" placeholder="post code"
-                                       class="form-control input-md" required>
+                                       class="form-control input-md">
                             </div>
                         </div>
 
@@ -113,7 +100,7 @@
                                 Region</label>
                             <div class="col-md-4">
                                 <input id="contact_phone_region" name="contact_phone_region" type="text"
-                                       placeholder="contact phone region" class="form-control input-md" required>
+                                       placeholder="contact phone region" class="form-control input-md">
                             </div>
                         </div>
 
@@ -121,7 +108,7 @@
                             <label class="col-md-4 control-label" for="contact_phone">Contact Phone</label>
                             <div class="col-md-4">
                                 <input id="contact_phone" name="contact_phone" type="text" placeholder="contact phone"
-                                       class="form-control input-md" required>
+                                       class="form-control input-md">
                             </div>
                         </div>
 
@@ -129,15 +116,15 @@
                             <label class="col-md-4 control-label" for="holiday_entitlement">Holiday Entitlement</label>
                             <div class="col-md-4">
                                 <input id="holiday_entitlement" name="holiday_entitlement" type="text" placeholder="holiday entitlement"
-                                       class="form-control input-md" required>
+                                       class="form-control input-md">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label" for="register"></label>
+                            <label class="col-md-4 control-label" for="update_employee"></label>
                             <div class="col-md-1">
-                                <button type="submit" value="Register" id="register" name="register"
-                                        class="btn btn-success">Register
+                                <button type="submit" value="update_employee" id="update_employee" name="update_employee"
+                                        class="btn btn-success">Update Employee
                                 </button>
                             </div>
                         </div>
