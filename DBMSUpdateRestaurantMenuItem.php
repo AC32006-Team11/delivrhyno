85393b605fd4ee7ab5568b00bd64708d46d10680
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-11-05 02:25:00 +0000

    update restaurant items

--- DBMSUpdateRestaurantMenuItem.php
+++ DBMSUpdateRestaurantMenuItem.php
@@ -1,7 +1,44 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Anthony
- * Date: 05/11/2016
- * Time: 00:47
- */
+<?php include 'header.php'; ?>
+
+<?php function updateRestaurantMenuItem()
+{
+
+    include 'dbConnect.php';
+
+    $restaurant_id = $_POST["restaurant_id"];
+    $restaurant_menu_item_id = $_POST["restaurant_menu_item_id"];
+    $menu_item_group = $_POST["menu_item_group"];
+    $menu_item_name = $_POST["menu_item_name"];
+    $menu_item_price = $_POST["menu_item_price"];
+
+    if (!empty($menu_item_group)) {
+        $query = "UPDATE `restaurant_menu_item` SET menu_item_group = '$menu_item_group' WHERE restaurant_menu_item_id = '$restaurant_menu_item_id' AND restaurant_id = '$restaurant_id' ";
+        $result = mysqli_query($db, $query) or die(mysqli_error($db));
+    }
+
+    if (!empty($menu_item_name)) {
+        $query = "UPDATE `restaurant_menu_item` SET menu_item_name = '$menu_item_name' WHERE restaurant_menu_item_id = '$restaurant_menu_item_id' AND restaurant_id = '$restaurant_id' ";
+        $result = mysqli_query($db, $query) or die(mysqli_error($db));
+    }
+
+    if (!empty($menu_item_price)) {
+        $query = "UPDATE `restaurant_menu_item` SET menu_item_price = '$menu_item_price' WHERE restaurant_menu_item_id = '$restaurant_menu_item_id' AND restaurant_id = '$restaurant_id' ";
+        $result = mysqli_query($db, $query) or die(mysqli_error($db));
+    }
+
+}
+
+if (isset($_POST["update_restaurant_menu_item"])) {
+    updateRestaurantMenuItem();
+
+    echo
+    '<div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Updated restaurant menu details</h1>
+                </div>
+            </div>
+        </div>';
+} ?>
+
+<?php include 'footer.php'; ?>
--- adminDBMSUpdateRestaurantMenuItem.php
+++ adminDBMSUpdateRestaurantMenuItem.php
@@ -1,7 +1,64 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Anthony
- * Date: 05/11/2016
- * Time: 00:27
- */
+<?php include 'header.php'; ?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <form name="Register" method="POST" action="DBMSUpdateRestaurantMenuItem.php" class="form-horizontal">
+                    <fieldset>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="restaurant_id">Restaurant ID</label>
+                            <div class="col-md-4">
+                                <input id="restaurant_id" name="restaurant_id" type="text"
+                                       placeholder="Select Restaurant via ID" class="form-control input-md" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="restaurant_menu_item_id">Item ID</label>
+                            <div class="col-md-4">
+                                <input id="restaurant_menu_item_id" name="restaurant_menu_item_id" type="text"
+                                       placeholder="Select Item via ID" class="form-control input-md" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="menu_item_group">Menu Item Group</label>
+                            <div class="col-md-4">
+                                <input id="menu_item_group" name="menu_item_group" type="text"
+                                       placeholder="Starters | Mains | Desert" class="form-control input-md">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="menu_item_name">Menu Item Name</label>
+                            <div class="col-md-4">
+                                <input id="menu_item_name" name="menu_item_name" type="text"
+                                       placeholder="Chicken" class="form-control input-md">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="menu_item_price">Menu Item Price</label>
+                            <div class="col-md-4">
+                                <input id="menu_item_price" name="menu_item_price" type="text"
+                                       placeholder="7.5" class="form-control input-md">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="update_restaurant_menu_item"></label>
+                            <div class="col-md-1">
+                                <button type="submit" value="update_restaurant_menu_item" id="update_restaurant_menu_item" name="update_restaurant_menu_item"
+                                        class="btn btn-success">Update Restaurant Menu Item
+                                </button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+
+<?php include 'footer.php'; ?>
