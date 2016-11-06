4e8e7148c90f2d4de24d39028cdf64f098641171
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-11-06 03:54:28 +0000

    initial beginnings of making a order

--- orderSelection.php
+++ orderSelection.php
@@ -2,15 +2,18 @@
 <?php include 'dbConnect.php'; ?>
 
 <?php
+
+$basketItemNameArray = array();
+
 if (isset($_GET['r'])) {
     $_SESSION['restaurantID'] = $_GET['r'];
 
-    $query = "SELECT * FROM `restaurant_menu_item` WHERE '$_SESSION[restaurantID]' = restaurant_id";
+    $query = "SELECT * FROM `restaurant_menu_item` WHERE '$_SESSION[restaurantID]' = restaurant_id ORDER BY menu_item_group DESC";
     $result = mysqli_query($db, $query) or die(mysqli_error($db));
 
     echo '<div class="container">
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-6">
                     <h3>Restaurant Menu</h3>';
 
     while ($row = mysqli_fetch_array($result)) {
@@ -20,11 +23,20 @@ if (isset($_GET['r'])) {
                     <th>' . $row[2] . '</th>
                     <th>' . $row[4] . '</th>
                     <th><a href="">Add item to basket</a></th>
-                </tr>';
-    }
-    echo '</table>
-            </div>
+                </tr>'; }
+
+        echo '</table>
                 </div>
-                    </div>'; } ?>
+                    <div class="col-md-6">
+                        <a href="orderPayment.php">Submit Order</a>
+                    </div>
+                    </div>
+                </div>'; } ?>
+
+<?php //code for basket
+
+?>
+
+
 
 <?php include 'footer.php'; ?>
