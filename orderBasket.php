da096ac2be7bad9bd88d3a5fc7b17db357eb0866
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-11-06 23:27:12 +0000

    beginnings of js basket

--- orderBasket.php
+++ orderBasket.php
@@ -1,62 +1,26 @@
-<?php include 'header.php';?>
-<?php include 'dbConnect.php'; ?>
+<?php
 
-    <!--$_SESSION['example'] = $row[1];
-    $_SESSION[example];-->
+session_start();
 
+if(isset($_POST['item_id']))
+{
+$_SESSION['id'][]=$_POST['item_id'];
+$_SESSION['name'][]=$_POST['item_name'];
+$_SESSION['price'][]=$_POST['item_price'];
+    echo count($_SESSION['name']);
+exit();
+}
 
-
-
-    <div class="container">
-
-        <?php
-        if ( isset($_SESSION["loggedIn"]) == "loggedIn") { ?>
-            <div class="row">
-                <div class="col-md-6" >
-                    <div style="border-radius: 20px;
-						background: #f8f8f8;
-						border-color:blue;
-						border: 4px solid #66CDAA;
-						padding-left:35px;
-						padding-right:35px;
-						padding-top:2px;
-						padding-bottom:15px;">
-                        <h2 style="text-align:center;">Your Basket</h2>
-                        <hr style="border-color:#e7e7e7;">
-                        <?php
-                        if ( isset($_SESSION["selectedItems"]) != null) {
-
-                            $arrlength=count($_SESSION[selectedItems]);
-                            for($x=0;$x<$arrlength;$x++)
-                            {?>
-
-                                <p><?php $selectedItems[$x]?></p>
-                                <?php
-                            }
-
-                        }  else {
-                            echo'<h1>No User Found</h1>';
-                        }
-                        ?>
-                    </div>
-                </div>
-                <div class="col-md-4 col-md-offset-1">
-
-                    <h2>Rhino facts of the day</h2>
-                    <hr>
-                    <p style="color:grey;"><i>The Indian Rhinoceros has a horn which can be up to 60 centimetres in length.</i></p>
-                    <br>
-                    <p style="color:grey;"><i>The Javan Rhinoceros is one of the most endangered large mammals in the world.</i></p>
-                    <br>
-                    <p style="color:grey;"><i>The Black Rhinoceros has 84 chromosomes.</i></p>
-
-
-
-                </div>
-            </div>
-        <?php }  else {
-            echo'<h1>No User Found</h1>';
-        } ?>
-    </div>
-
-<?php include 'footer.php';?>
+if(isset($_POST['showcart']))
+{
+for($i=0;$i<count($_SESSION['id']);$i++)
+{
+echo "<div class='cart_items'>";
+    echo "<p>".$_SESSION['id'][$i]."</p>";
+    echo "<p>".$_SESSION['name'][$i]."</p>";
+    echo "<p>".$_SESSION['price'][$i]."</p>";
+    echo "</div>";
+}
+exit();
+}
+?>
--- orderSelection.php
+++ orderSelection.php
@@ -1,6 +1,65 @@
 <?php include 'header.php'; ?>
 <?php include 'dbConnect.php'; ?>
 
+<script type="text/javascript">
+
+    $(document).ready(function () {
+
+        $.ajax({
+            type: 'post',
+            url: 'orderBasket.php',
+            data: {
+                total_cart_items: "totalitems"
+            },
+            success: function (response) {
+                document.getElementById("total_items").value = response;
+            }
+        });
+
+    });
+
+    function cart(id) {
+        var ele = document.getElementById(id);
+        var name = document.getElementById("item_name").value;
+        var price = document.getElementById("item_price").value;
+
+        $.ajax({
+            type: 'post',
+            url: 'orderBasket.php',
+            data: {
+                item_id: ele,
+                item_name: name,
+                item_price: price
+            },
+            success: function (response) {
+                document.getElementById("total_items").value = response;
+            }
+        });
+
+    }
+
+    function show_cart() {
+        $.ajax({
+            type: 'post',
+            url: 'orderBasket.php',
+            data: {
+                showcart: "cart"
+            },
+            success: function (response) {
+                document.getElementById("mycart").innerHTML = response;
+                $("#mycart").slideToggle();
+            }
+        });
+    }
+</script>
+
+<b id="cart_button" onclick="show_cart();">
+    <input type="button" id="total_items" value="">
+</b>
+
+<div id="mycart">
+</div>
+
 <?php
 
 //cannot load with different menu if navigate back
@@ -8,22 +67,24 @@ if (empty($_SESSION['restaurantID']) or !empty($_SESSION['restaurantID'])) {
     $_SESSION['restaurantID'] = $_GET['r'];
 }
 
-    $query = "SELECT * FROM `restaurant_menu_item` WHERE '$_SESSION[restaurantID]' = restaurant_id ORDER BY menu_item_group DESC";
-    $result = mysqli_query($db, $query) or die(mysqli_error($db));
+$query = "SELECT * FROM `restaurant_menu_item` WHERE '$_SESSION[restaurantID]' = restaurant_id ORDER BY menu_item_group DESC";
+$result = mysqli_query($db, $query) or die(mysqli_error($db));
 
-    echo '<div class="container">
+echo '<div class="container">
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-8">
                     <h3>Restaurant Menu</h3>';
 
-    while ($row = mysqli_fetch_array($result)) {
-        echo '<table class="table">
+while ($row = mysqli_fetch_array($result)) {
+    echo '<table class="table">
+                <div class="items" id='.$row[0].'>
                 <tr>
-                    <th>' . $row[3] . '</th>
+                    <th><img src="assets\basket.png"></th>
+                    <th><input type="hidden" id="item_name" value='.$row[3].'>' . $row[3] . '</th>
                     <th>' . $row[2] . '</th>
-                    <th>' . $row[4] . '</th>
-                    <th><a href="orderSelection.php?i='.$row[0].'">Add item to basket</a></th>
-                </tr>'; } echo '</table>' ?>
-
+                    <th><input type="hidden" id="item_price" value='.$row[4].'>' . $row[4] . '</th>
+                    <th><input type="button" value="Add To CART" onclick="cart('.$row[0].')"></th>
+                </tr>'; } echo '</table></div></div></div>'?>
 
 <?php include 'footer.php'; ?>
+
