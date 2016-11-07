8ccd8581e480b296cde88eb6dfca3196b5c23a44
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-11-07 13:34:19 +0000

    remove button

--- orderBasket.php
+++ orderBasket.php
@@ -2,13 +2,21 @@
 
 session_start();
 
+if(isset($_POST['removecart'])) {
+    $unsetKey= $_POST['removecart'];
+    unset($_SESSION['id'][$unsetKey]);
+    unset($_SESSION['name'][$unsetKey]);
+    unset($_SESSION['price'][$unsetKey]);
+    exit();
+}
+
 if(isset($_POST['total_cart_items']))
 {
     echo count($_SESSION['id']);
     exit();
 }
 
-if(isset($_POST['item_id']))
+if(isset($_POST['item_name']))
 {
 $_SESSION['id'][]=$_POST['item_id'];
 $_SESSION['name'][]=$_POST['item_name'];
@@ -25,6 +33,7 @@ echo "<div class='cart_items'>";
     echo "<b>".$_SESSION['id'][$i]."</b>";
     echo "<b>".$_SESSION['name'][$i]."</b>";
     echo "<b>".$_SESSION['price'][$i]."</b>";
+    echo "<input type='button' value='Remove from CART' onclick='removeCart($i)'>";
     echo "</div>";
 }
 exit();
--- orderSelection.php
+++ orderSelection.php
@@ -49,6 +49,21 @@
             }
         });
     }
+
+    function removeCart(item_id) {
+        var ele = document.getElementById(item_id);
+
+        $.ajax({
+            type: 'post',
+            url: 'orderBasket.php',
+            data: {
+                removecart: ele
+            },
+            success: function (response) {
+                document.getElementById("total_items").value = response;
+            }
+        });
+    }
 </script>
 
 <b id="cart_button" onclick="show_cart();">
@@ -59,29 +74,43 @@
 </div>
 
 <?php
-
 //cannot load with different menu if navigate back
 if (empty($_SESSION['restaurantID']) or !empty($_SESSION['restaurantID'])) {
     $_SESSION['restaurantID'] = $_GET['r'];
 }
-
-$query = "SELECT * FROM `restaurant_menu_item` WHERE '$_SESSION[restaurantID]' = restaurant_id ORDER BY menu_item_group DESC";
-$result = mysqli_query($db, $query) or die(mysqli_error($db));
-
+$extraquery = "SELECT DISTINCT `menu_item_group` FROM `restaurant_menu_item` WHERE '$_SESSION[restaurantID]' = restaurant_id ORDER BY menu_item_group DESC";
+$extraresult = mysqli_query($db, $extraquery) or die(mysqli_error($db));
+$subcount = mysqli_num_rows($extraresult);
+echo $subcount;
 echo '<div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <h3>Restaurant Menu</h3>';
-
-while ($row = mysqli_fetch_array($result)) {
-    echo '<table class="table">
-                <tr>
-                    <th><img src="assets\basket.png"></th>
-                    <th><input type="hidden" id="item_name_'.$row[0].'" value="'.$row[3].'">' . $row[3] . '</th>
-                    <th>' . $row[2] . '</th>
-                    <th><input type="hidden" id="item_price_'.$row[0].'" value="'.$row[4].'">' . $row[4] . '</th>
-                    <th><input type="button" value="Add To CART" onclick="cart('.$row[0].')"></th>
-                </tr>'; } echo '</table></div></div>'?>
+            <div class="col-md-12">
+                <div class="thumbnail">
+                    <h3>Restaurant Menu</h3>
+                    <h4>Restaurant id = ' . $_SESSION['restaurantID'] . '</h4>
+                </div>
+            </div>
+            <div class="col-md-12">';
+while($menurow = mysqli_fetch_array($extraresult)){
+    echo '<div class="thumbnail">
+                    <h2>' . $menurow[0] . '</h2>';
+    $query = "SELECT * FROM `restaurant_menu_item` WHERE '$_SESSION[restaurantID]' = restaurant_id AND menu_item_group ='$menurow[0]'";
+    $result = mysqli_query($db, $query) or die(mysqli_error($db));
+    while($row = mysqli_fetch_array($result)){
+        echo '<div class="thumbnail">
+                   <table class="table">
+                        <tr>
+                            <th><img src="assets\basket.png"></th>
+                            <th><input type="hidden" id="item_name_'.$row[0].'" value="'.$row[3].'"></th>
+                            <th><input type="hidden" id="item_price_'.$row[0].'" value="'.$row[4].'"></th>
+                            <th><p>' . $row[3] . '</p></th>
+                            <th><p>' . $row[2] . '</p></th>
+                            <th><p>' . $row[4] . '</p></th>
+                            <th><input type="button" value="Add To CART" onclick="cart('.$row[0].')"></th>
+                        </tr>
+                   </table>
+               </div>';}
+    echo '</div>';}
+echo ' </div>'?>
 
 <?php include 'footer.php'; ?>
 
