43433a66d56c4ca6f1cf829a17ea344b22b6d9e7
IainMorton <IainMorton@computing.dundee.ac.uk>
2016-11-08 17:15:52 +0000

    Addition of fifth Query

--- DBMSAdvancedQuery5.php
+++ DBMSAdvancedQuery5.php
@@ -1,17 +1,16 @@
 <?php include 'header.php'; ?>
 
-<?php function advancedQuery4()
+<?php function advancedQuery5()
 {
 
     include 'dbConnect.php';
-    $type = $_GET["type"];
-    $city = $_GET["city"];
+ 
+  
     ?>
     <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h2 style="text-align:center;">You have searched all restaurants in <?php echo "$city" ?> which do not serve
-                the <?php echo "$type" ?> style of food</h2>
+            <h2 style="text-align:center;">You have searched for all employees that earn more than the average salary</h2>
             <br>
         </div>
     </div>
@@ -20,17 +19,19 @@
     <table class="table table-striped table-bordered table-condensed" style="width:100%";>
     <thead>
     <tr>
-        <th>Restaurant Name</th>
-        <th>Restaurant Type</th>
+        <th>Forename</th>
+        <th>Surname</th>
+		<th>Role</th>
+		<th>Salary</th>
 
 
     </tr>
     </thead>
 
     <?php
-    if (!empty($city)) {
-        $query = "SELECT restaurant_name, restaurant_description FROM restaurant
-					WHERE restaurant_description not in (SELECT '$type' FROM restaurant) AND city='$city' ORDER BY restaurant_name ASC;";
+    
+        $query = "SELECT forename, surname, role, salary 
+					FROM employee, payroll having salary>(SELECT AVG(SALARY) FROM payroll) ORDER BY surname ASC;";
         $result = mysqli_query($db, $query) or die(mysqli_error($db));
         while ($row = mysqli_fetch_array($result)) {
             echo '
@@ -38,16 +39,18 @@
 				
                     <td>' . $row[0] . '</td>
                     <td>' . $row[1] . '</td>
+					<td>' . $row[2] . '</td>
+                    <td>' . $row[3] . '</td>
                     
                 </tbody>';
         }
-    }
+    
 
 
 }
 
 if (isset($_GET["performquery"])) {
-    advancedQuery4();
+    advancedQuery5();
 
 
 } ?>
