143d15cc522234a76bf2b1551547266838390f1c
Anthony Grimes <anthony.94.grimes@gmail.com>
2016-11-09 18:44:19 +0000

    basket changes and prepared statement on assing

--- orderSelection.php
+++ orderSelection.php
@@ -68,7 +68,7 @@
 </script>
 
 <b id="cart_button" onclick="show_cart();">
-    <input type="button" id="total_items" value="">
+    <input type="button" id="total_item s" value="Show/Hide My Basket">
 </b>
 
 <div id="mycart">
@@ -82,28 +82,21 @@ if (empty($_SESSION['restaurantID']) or !empty($_SESSION['restaurantID'])) {
 $extraquery = "SELECT DISTINCT menu_item_group FROM `restaurant_menu_item` WHERE '$_SESSION[restaurantID]' = restaurant_id ORDER BY menu_item_group DESC";
 $extraresult = mysqli_query($db, $extraquery) or die(mysqli_error($db));
 $subcount = mysqli_num_rows($extraresult);
-echo $subcount;
+
 echo '<div class="container">
-            <div class="col-md-12">
-                <div class="thumbnail">
-                    <h3>Restaurant Menu</h3>
-                    <h4>Restaurant id = ' . $_SESSION['restaurantID'] . '</h4>
-                    <a href="orderPayment.php">Submit your order</a>
-                </div>
-            </div>
             <div class="col-md-12">';
 while($menurow = mysqli_fetch_array($extraresult)){
-    echo '<div class="thumbnail">
-                    <h2>' . $menurow[0] . '</h2>';
+    echo '<div class="panel panel-default">
+                    <div class="panel-heading"><h2>' . $menurow[0] . '</h2></div>';
     $query = "SELECT * FROM `restaurant_menu_item` WHERE '$_SESSION[restaurantID]' = restaurant_id AND menu_item_group ='$menurow[0]'";
     $result = mysqli_query($db, $query) or die(mysqli_error($db));
     while($row = mysqli_fetch_array($result)) {
-        echo '<div class="thumbnail">
+        echo '<div class="panel panel-body">
                 <div class="row">
                    <div class = "col-md-3">
-                    <img src="assets\basket.png">
-                    <input type="hidden" id="item_name_'.$row[0].'" value="'.$row[3].'">
-                    <input type="hidden" id="item_price_'.$row[0].'" value="'.$row[4].'">
+                    <span class="glyphicon glyphicon-shopping-cart" style="font-size: 1.75em;"></span>
+                    <input type="hidden" style="vertical-align: middle; display: table-cell;"id="item_name_'.$row[0].'" value="'.$row[3].'">
+                    <input type="hidden" style="vertical-align: middle; display: table-cell;"id="item_price_'.$row[0].'" value="'.$row[4].'">
                    </div>
                    <div class = "col-md-3">
                     <p>' . $row[3] . '</p>
@@ -112,7 +105,7 @@ while($menurow = mysqli_fetch_array($extraresult)){
                     <p>' . $row[4] . '</p>
                    </div>
                    <div class = "col-md-3">
-                    <input type="button" value="Add To CART" onclick="cart('.$row[0].')">
+                    <button type="button" class="btn btn-primary" value="Add to CART" onclick="cart('.$row[0].')">Add to Cart</button>
                    </div>
                </div>
               </div>';}
@@ -120,4 +113,3 @@ while($menurow = mysqli_fetch_array($extraresult)){
 echo ' </div>'?>
 
 <?php include 'footer.php'; ?>
-
