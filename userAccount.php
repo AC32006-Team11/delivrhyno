3d3ac647dd46db1503989cfb081accbcb1741524
Craigieboy10 <craig-watt@live.co.uk>
2016-11-09 18:09:10 +0000

    Merge branch 'anthonyagainwowo' of https://github.com/Team11-AC32006-AC52001/delivrhyno into cwwwww
    # Conflicts:
    #	header.php
    #	orderBasket.php
    #	orderCreate.php
    #	orderSelection.php

--- userAccount.php
+++ userAccount.php
@@ -3,24 +3,44 @@
 
 <?php
 if (isset($_SESSION["loggedIn"]) == "loggedIn") {
-    $query = "SELECT * FROM `customer` WHERE '$_SESSION[username]' = username";
-    $result = mysqli_query($db, $query) or die(mysqli_error($db));
+        $currentUsername = $_SESSION['username'];
+        if ($stmt = $db->prepare("SELECT title, forename, surname, street, county, city, post_code, contact_phone_region, contact_phone, contact_email FROM customer WHERE username = ?")) {
+            $stmt->bind_param("s", $currentUsername);
+            $stmt->execute();
+            $stmt->bind_result($title, $forename, $surname, $street, $county, $city, $post_code, $contact_phone_region, $contact_phone, $contact_email);
+            while ($stmt->fetch()) {
+                $_SESSION['title'] = $title;
+                $_SESSION['forename'] = $forename;
+                $_SESSION['surname'] = $surname;
+                $_SESSION['street'] = $street;
+                $_SESSION['county'] = $county;
+                $_SESSION['city'] = $city;
+                $_SESSION['post_code'] = $post_code;
+                $_SESSION['contact_phone_region'] = $contact_phone_region;
+                $_SESSION['contact_phone'] = $contact_phone;
+                $_SESSION['contact_email'] = $contact_email;
+            }
+            $stmt->close();
+        }
 
-    $row = mysqli_fetch_array($result);
+    //$query = "SELECT * FROM `customer` WHERE '$_SESSION[username]' = username";
+    //$result = mysqli_query($db, $query) or die(mysqli_error($db));
 
-    $count = mysqli_num_rows($result);
-    if ($count == 1) {
-        $_SESSION['title'] = $row[1];
-        $_SESSION['forename'] = $row[2];
-        $_SESSION['surname'] = $row[3];
-        $_SESSION['street'] = $row[4];
-        $_SESSION['county'] = $row[5];
-        $_SESSION['city'] = $row[6];
-        $_SESSION['post_code'] = $row[7];
-        $_SESSION['contact_phone_region'] = $row[8];
-        $_SESSION['contact_phone'] = $row[9];
-        $_SESSION['contact_email'] = $row[10];
-    }
+    //$row = mysqli_fetch_array($result);
+
+    //$count = mysqli_num_rows($result);
+    //if ($count == 1) {
+      //  $_SESSION['title'] = $row[1];
+      //  $_SESSION['forename'] = $row[2];
+      //  $_SESSION['surname'] = $row[3];
+      //  $_SESSION['street'] = $row[4];
+      //  $_SESSION['county'] = $row[5];
+      //  $_SESSION['city'] = $row[6];
+      //  $_SESSION['post_code'] = $row[7];
+      //  $_SESSION['contact_phone_region'] = $row[8];
+      //  $_SESSION['contact_phone'] = $row[9];
+       // $_SESSION['contact_email'] = $row[10];
+    //}
 
     ?>
     <div class="container">
@@ -48,8 +68,4 @@ if (isset($_SESSION["loggedIn"]) == "loggedIn") {
             <div class="col-md-4"></div>
         </div>
     </div>
-<?php } else {
-    echo '<h2>No User Found</h2>';
-} ?>
-
-<?php include 'footer.php'; ?>
+<?php }include 'footer.php'; ?>
