d698630f63f04808a8eabe72de1f8621ac2cb193
IainMorton <IainMorton@computing.dundee.ac.uk>
2016-11-09 14:09:55 +0000

    Added redirects to the homepage when a login or a logout is completed

--- userLogout.php
+++ userLogout.php
@@ -1,5 +1,5 @@
 <?php include 'header.php'; ?>
-
+<meta http-equiv="refresh" content="2; url="index.php" />
 <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
@@ -10,6 +10,7 @@ if ( isset($_SESSION["loggedIn"]) == "loggedIn") {
     session_destroy();
 
     echo "<h2 style='text-align:center;'>You have been successfully logged out</h2>";
+	echo "<h2 style='text-align:center;'>Redirecting you to the home page...</h2>";
 } else {
     echo "<h2 style='text-align:center;'>No user logged in</h2>";
 } ?>
