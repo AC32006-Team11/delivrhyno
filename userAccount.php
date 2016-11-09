4dca8075ded7342ef9f91ed24a2145f4a00846ba
Craigieboy10 <craig-watt@live.co.uk>
2016-11-09 18:10:35 +0000

    Merge branch 'anthonyagainwowo' of https://github.com/Team11-AC32006-AC52001/delivrhyno into cwwwww
    # Conflicts:
    #	header.php
    #	orderBasket.php
    #	orderCreate.php
    #	orderSelection.php

--- userAccount.php
+++ userAccount.php
@@ -22,26 +22,6 @@ if (isset($_SESSION["loggedIn"]) == "loggedIn") {
             }
             $stmt->close();
         }
-
-    //$query = "SELECT * FROM `customer` WHERE '$_SESSION[username]' = username";
-    //$result = mysqli_query($db, $query) or die(mysqli_error($db));
-
-    //$row = mysqli_fetch_array($result);
-
-    //$count = mysqli_num_rows($result);
-    //if ($count == 1) {
-      //  $_SESSION['title'] = $row[1];
-      //  $_SESSION['forename'] = $row[2];
-      //  $_SESSION['surname'] = $row[3];
-      //  $_SESSION['street'] = $row[4];
-      //  $_SESSION['county'] = $row[5];
-      //  $_SESSION['city'] = $row[6];
-      //  $_SESSION['post_code'] = $row[7];
-      //  $_SESSION['contact_phone_region'] = $row[8];
-      //  $_SESSION['contact_phone'] = $row[9];
-       // $_SESSION['contact_email'] = $row[10];
-    //}
-
     ?>
     <div class="container">
         <div class="row">
