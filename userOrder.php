e83e36f9a09421fc831e2250974d14ada32d1fc9
Anthony Grimes <contact@anthonygrimes.co.uk>
2016-11-10 20:23:17 +0000

    bugfixes

--- userOrder.php
+++ userOrder.php
@@ -5,55 +5,35 @@ if (isset($_SESSION["loggedIn"]) == "loggedIn") { ?>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h2>Your deliveries</h2>
+                <h2>Your Orders</h2>
                 <table class="table table-condensed">
-                    <thead>
-                    <tr>
-                        <th>Order ID</th>
-                        <th>Ordered From</th>
-                        <th>Ordered At</th>
-                        <th>Items</th>
-                        <th>Total Cost</th>
-                        <th>Address</th>
-                    </tr>
-                    </thead>
-					<p><?php echo("{$_SESSION['customerID']}"."<br />");?></p>
-                <?php
-					
-					
+
+                    <?php
+
+                    echo "<thead><tr><th>Order ID</th><th>Ordered From</th><th>Ordered At</th><th>Items</th><th>Total Cost</th><th>Address</th></tr></thead>";
+
                     $queryTransaction = "SELECT * FROM transaction WHERE customer_id = '$_SESSION[customerID]'";
                     $resultTransaction = mysqli_query($db, $queryTransaction) or die(mysqli_error($db));
 
-                    $rowTransaction = mysqli_fetch_array($resultTransaction);
-
-                    $queryAddress = "SELECT * FROM customer WHERE customer_id = $rowTransaction[1]";
+                    $queryAddress = "SELECT * FROM customer WHERE customer_id = '$_SESSION[customerID]'";
                     $resultAddress = mysqli_query($db, $queryAddress) or die(mysqli_error($db));
 
                     $rowAddress = mysqli_fetch_array($resultAddress);
 
-                    $queryRestaurant = "SELECT * FROM restaurant WHERE restaurant_id = '$rowTransaction[0]'";
-                    $resultRestaurant = mysqli_query($db, $queryRestaurant) or die(mysqli_error($db));
+                    while ($row = mysqli_fetch_array($resultTransaction)) {
+                        //$rowTransaction = mysqli_fetch_array($resultTransaction);
 
-                    $rowRestaurant = mysqli_fetch_array($resultRestaurant);
+                        //$rowRestaurant = mysqli_fetch_array($resultRestaurant);
 
-                    $queryPayment = "SELECT * FROM transaction_payment WHERE transaction_id = '$rowTransaction[0]'";
-                    $resultPayment = mysqli_query($db, $queryPayment) or die(mysqli_error($db));
-
-                    $rowPayment = mysqli_fetch_array($resultPayment);
-
-                    $_SESSION['orderItems'] = $rowTransaction[5];
-                    $_SESSION['orderCost'] = $rowPayment[2];
-                    $_SESSION['orderedFrom'] = $rowRestaurant[1];
-                    $_SESSION['orderedAt'] = $rowTransaction[4];
-                    $_SESSION['street'] = $rowAddress[4];
-                    $_SESSION['county'] = $rowAddress[5];
-                    $_SESSION['city'] = $rowAddress[6];
-                    $_SESSION['post_code'] = $rowAddress[7];
-
-
-                    echo "<tbody><td>".$rowTransaction[0]."</td><td>$_SESSION[orderedFrom]</td><td>$_SESSION[orderedAt]</td><td>$_SESSION[orderItems]</td><td>$_SESSION[orderCost]</td><td>$_SESSION[street], $_SESSION[county], $_SESSION[city], $_SESSION[post_code]</td>";
-                    echo "</tbody></table>";
-                ?>
+                        echo "<tr><td>".$row["0"]."</td>";
+                        echo "<td>'Fix'</td>"; // ordered from
+                        echo "<td>".$row["4"]."</td>";
+                        echo "<td>".$row["5"]."</td>";
+                        echo "<td>'Fix'</td>"; // total cost
+                        echo "<td>".$rowAddress["4"]." ".$rowAddress["5"]." ".$rowAddress["6"]." ".$rowAddress["7"]."</td>"; // cust address
+                    }
+                    ?>
+                    <?php echo "</tr></tbody></table>"; ?>
 
             </div>
         </div>
