a30c104643ac17025a8b4a087edd7e1587fb8541
cjgsdundee <SutherlandCJG@outlook.com>
2016-11-10 16:44:41 +0000

    admin login working properly, we are now done! (apparently)

--- adminLogin.php
+++ adminLogin.php
@@ -3,41 +3,57 @@
 <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-<?php
-include('dbConnect.php');
+
+				<?php
+				include('dbConnect.php');
 
 
-//still requires role filtering
-if (isset($_POST['username']) and isset($_POST['password'])) {
+				//still requires role filtering
+				if (isset($_POST['username']) and isset($_POST['password'])) {
 
-    $username = $_POST['username'];
-    $passwordInsecure = $_POST["password"];
-    $password = sha1($passwordInsecure);
+					$username = $_POST['username'];
+					$passwordInsecure = $_POST["password"];
+					$password = sha1($passwordInsecure);
 
-    $query = "SELECT * FROM `employee` WHERE username = '$username' and password = '$password' and role = 'ADM'";
-    $result = mysqli_query($db, $query) or die(mysqli_error($db));
+					if (!empty($username)) {
+						$result=mysqli_prepare($db,"SELECT role FROM `employee` WHERE (username = ? and password=?)");
+						mysqli_stmt_bind_param($result,'ss',$username,$password);
+						mysqli_stmt_execute($result);
+						mysqli_stmt_store_result($result);
+						mysqli_stmt_bind_result($result,$roleID);
+					}
 
-    $row = mysqli_fetch_array($result);
+					$count = mysqli_stmt_num_rows($result);
+					if ($count == 1) {
+						while (mysqli_stmt_fetch($result)) {
+							if ($roleID == "ADM") {
+								$_SESSION['roleID'] = $roleID;
+								$_SESSION['admLoggedIn'] = "admLoggedIn";
+								$_SESSION['username'] = $username;
 
-    $count = mysqli_num_rows($result);
-    if ($count == 1) {
-        $_SESSION['admLoggedIn'] = "admLoggedIn";
-        $_SESSION['username'] = $username;
-        echo "<h2 style='text-align:center;'>Valid Login Credentials</h2>";
-		
-    }
-	else 
-	{
-		echo "<h2 style='text-align:center;'>Invalid Login Credentials</h2>";
-	}
-	echo "<h2 style='text-align:center;'>Redirecting you to the home page...</h2>";
-	?><script>
-    setTimeout(function(){location.href="index.php"} , 3000);
-	</script><?php
-} 
-?>
+							} else {
+								$_SESSION['roleID'] = $roleID;
+								$_SESSION['admLoggedIn'] = "admLoggedIn";
+								$_SESSION['username'] = $username;
+							}
+						}
+
+
+						echo "<h2 style='text-align:center;'>Valid Login Credentials</h2>";
+
+					}
+					else
+					{
+						echo "<h2 style='text-align:center;'>Invalid Login Credentials</h2>";
+					}
+					echo "<h2 style='text-align:center;'>Redirecting you to the home page...</h2>";
+					?><script>
+						setTimeout(function(){location.href="index.php"} , 3000);
+					</script><?php
+				}
+				?>
+			</div>
 		</div>
-	</div>
 </div>
 
 <?php include 'footer.php'; ?>
